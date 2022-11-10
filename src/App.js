8a9684616593c8cb3a20258841d974181a299582
Thamasha-ru <thamasharu@gmail.com>
2022-11-10 23:19:38 +0530

    NavBarChanges

--- src/App.js
+++ src/App.js
@@ -79,9 +79,10 @@ const App = () => {
   return (  <>
   <div class="topnav">
     <a class="active" href="#home">Home</a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
     <a href="#about">About</a>
+    <a href="#contact">Contact</a>
+    <a href="#signup">Signup</a>
+    <a href="#login">Login</a>
   </div><div className="app">
       <div className="app__left">
         <div className="app__header">
